245da830dec2308f9c53b5f40595b1d9d8dc0722
eriklofgren75 <eriklofgren75@gmail.com>
2025-09-27 20:06:00 -0600

    Week 02

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,20 @@
-import { getParkData } from "./parkService.mjs";
-
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 const parkData = getParkData();
 
-function parkInfoTemplate(info) {
-  return `<a href="/" class="hero-banner__title">${info.name}</a>
-  <p class="hero-banner__subtitle">
-    <span>${info.designation}</span>
-    <span>${info.states}</span>
-  </p>`;
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${parkData.fullName}</h1>
+  <p>${parkData.description}</p>`;
 }
 
-// insert data into disclaimer section
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
-// update the title of the site. Notice that we can select things in the head just like in the body with querySelector
-document.querySelector("head > title").textContent = parkData.fullName;
-// set the banner image
-document.querySelector(".hero-banner > img").src = parkData.images[0].url;
-// use the template function above to set the rest of the park specific info in the header
-document.querySelector(".hero-banner__content").innerHTML =
-  parkInfoTemplate(parkData);
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
+
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
